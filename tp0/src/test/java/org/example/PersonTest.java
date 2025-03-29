c88b3f439dc0b456d674ee3f1d75de8e8dc7005e
ZAKARIA BOUGADI <zakariabougadi@gmail.com>
2025-03-29 15:01:41 +0100

    29 mars commit

--- tp0/src/test/java/org/example/PersonTest.java
+++ tp0/src/test/java/org/example/PersonTest.java
@@ -1,16 +1,31 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
     @Test
-    void getFullName() {
+    void getFullNameShouldReturnConcatenatedName() {
+        Person person = new Person("Zakaria", "bougadi", 25);
+        assertEquals("Zakaria bougadi", person.getFullName());
     }
 
     @Test
-    void isAdult() {
+    void isAdultShouldReturnFalseIfAgeIsLessThan18() {
+        Person teen = new Person("djeloul", "bari", 17);
+        assertFalse(teen.isAdult());
+    }
+
+    @Test
+    void isAdultShouldReturnTrueIfAgeIs18() {
+        Person justAdult = new Person("hadji", "issam", 18);
+        assertTrue(justAdult.isAdult());
+    }
+
+    @Test
+    void isAdultShouldReturnTrueIfAgeIsGreaterThan18() {
+        Person adult = new Person("zwatin", "hmida", 30);
+        assertTrue(adult.isAdult());
     }
 }
