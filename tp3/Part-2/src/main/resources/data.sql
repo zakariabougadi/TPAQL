d34e50202a91b6f19218f09b0a6de6a8291acab5
ZAKARIA BOUGADI <zakariabougadi@gmail.com>
2025-05-23 07:41:19 +0100

    23th commit

--- tp3/Part-2/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
--- Insertion de données de test
-INSERT INTO tasks (title, description, created_at, completed)
-VALUES 
-    ('Tâche initiale 1', 'Description de la tâche 1', NOW(), false),
-    ('Tâche initiale 2', 'Description de la tâche 2', NOW(), true); 
