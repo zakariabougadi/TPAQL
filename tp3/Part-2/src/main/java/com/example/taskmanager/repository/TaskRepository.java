801793174617f8e6eb365d89b7478aa30309c890
ZAKARIA BOUGADI <zakariabougadi@gmail.com>
2025-05-23 07:41:19 +0100

    23th commit

--- tp3/Part-2/src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.taskmanager.repository;
-
-import com.example.taskmanager.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-} 
